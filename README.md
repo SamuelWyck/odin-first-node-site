4c6c33613ae9ea3c63e36c699055319f47561dba
samuel <samawyck@gmail.com>
2025-05-27 12:58:59 -0500

    Create site

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # odin-first-node-site
+
+A simple website to practice node.
---
+++ index.js
@@ -0,0 +1,28 @@
+import url from "url";
+import http from "http";
+import fs from "fs";
+
+
+function requestHandler(req, res) {
+    const urlObject = url.parse(req.url, true);
+    let fileName = `./templates/${urlObject.pathname}`;
+    fileName = (urlObject.pathname === "/") ? "./templates/index.html" : fileName;
+    fileName = (urlObject.pathname === "/styles.css") ? "./styles.css" : fileName;
+
+    fs.readFile(fileName, function(error, data) {
+        if (error) {
+            return fs.readFile("./templates/404.html", function(err, data) {
+                res.writeHeader(404, {"content-type": "text/html"});
+                res.write(data);
+                return res.end();
+            })
+        }
+
+        res.writeHeader(200, {"content-type": "text/html"});
+        res.write(data);
+        return res.end();
+    });
+};
+
+
+http.createServer(requestHandler).listen(8080);
